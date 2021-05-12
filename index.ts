00d90b9950b010247a8fcb0b2228b3e9fee09813
Matheus Alencar <matheus@kleros.io>
2021-05-11 23:36:50 -0300

    wip: add logic to build csv

--- index.ts
+++ index.ts
@@ -1,13 +1,28 @@
-#!/usr/bin/env node
-'use strict';
-
-
 // This tool builds a csv of all your payments and penalties your accounts received.
 import { ethers } from 'ethers';
-import { ArgumentParser, RawDescriptionHelpFormatter, RawTextHelpFormatter } from 'argparse';
+import { ArgumentParser } from 'argparse';
+import fs from 'fs'
+import R from 'ramda'
+import json2csv from 'json2csv'
+import fetch from 'node-fetch';
 
 import { version } from './package.json';
 import klerosABI from './kleros-abi.json'
+
+function toCSV(records, fileName) {
+  if (!records.length) throw 'Error - Array is empty';
+
+  const fields = {data: records, fields: R.keys(records[0])};
+
+  return new Promise<void>(function(resolve, reject) {
+    json2csv(fields, function(err, csv) {
+      if (err) reject(err);
+      fs.writeFile(fileName, csv, function(err) {
+        return !err ? resolve() : reject(err);
+      });
+    });
+  });
+};
 
 
 const parser = new ArgumentParser({
@@ -35,12 +50,57 @@ const provider = ethers.providers.getDefaultProvider('mainnet', {
   etherscan: etherscanKey
 })
 const KLEROS_ADDRESS = '0x988b3A538b618C7A603e1c11Ab82Cd16dbE28069'
+const PNK_ADDRESS= '0x93ed3fbe21207ec2e8f2d3c3de6e058cb73bc04d'
+
 const klerosLiquid = new ethers.Contract(KLEROS_ADDRESS, klerosABI, provider)
 
 // We do things synchronously and add delays to avoid hitting rate limits.
 ;(async () => {
   for (const jurorAddress of jurorAddresses) {
-    // Pull data
-    console.info(jurorAddress)
+    console.info('Fetching events...')
+    const tokenAndEthShiftEvents = (await provider.getLogs({
+      ...klerosLiquid.filters.TokenAndETHShift(jurorAddress),
+      fromBlock: 7303699,
+      toBlock: 'latest'
+    }))
+    console.info('Fetching prices...')
+
+    const results = [];
+    for (const tokenAndEthShiftEvent of tokenAndEthShiftEvents) {
+      const { timestamp } = await provider.getBlock(tokenAndEthShiftEvent.blockNumber)
+      const date = new Date(timestamp * 1000)
+      const dateString = `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`
+
+      const BASIS_POINTS = 10000
+      const pnkPriceUsdAtTheTime = ethers.utils.parseUnits(String((await (await fetch(`https://api.coingecko.com/api/v3/coins/kleros/history?date=${dateString}`)).json()).market_data.current_price.usd*BASIS_POINTS), 2)
+      const etherPriceUsdAtTheTime = ethers.utils.parseUnits(String((await (await fetch(`https://api.coingecko.com/api/v3/coins/ethereum/history?date=${dateString}`)).json()).market_data.current_price.usd*BASIS_POINTS), 2)
+
+      const parsedEvent = klerosLiquid.interface.parseLog(tokenAndEthShiftEvent)
+
+      // Note that this value can be negative if the juror was punished.
+      const pnkGain = parsedEvent.args._tokenAmount as ethers.BigNumber;
+      const pnkGainInUsd = pnkGain.mul(pnkPriceUsdAtTheTime)
+
+      const ethGain = parsedEvent.args._ETHAmount as ethers.BigNumber;
+      const ethGainInUsd = ethGain.mul(etherPriceUsdAtTheTime)
+
+      results.push({
+        'Amount': ethers.utils.formatUnits(pnkGain, 18),
+        'Currency': 'PNK',
+        'Value in USD at the time': ethers.utils.formatUnits(pnkGainInUsd.div(ethers.BigNumber.from(BASIS_POINTS)), 18),
+        'Date': dateString,
+        'Tx Hash': tokenAndEthShiftEvent.transactionHash
+      })
+      results.push({
+        'Amount': ethers.utils.formatUnits(ethGain, 18),
+        'Currency': 'ETH',
+        'Value in USD at the time': ethers.utils.formatUnits(ethGainInUsd.div(ethers.BigNumber.from(BASIS_POINTS)), 18),
+        'Date': dateString,
+        'Tx Hash': tokenAndEthShiftEvent.transactionHash
+      })
+    }
+
+    console.info('Building csv...')
+    toCSV(results, 'output.csv');
   }
 })()
